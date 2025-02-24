24a15a026615bec8e53497b6b604e109b9539e08
Runtime-Error3490 <shaleen.shukla@learner.manipal.edu>
2025-02-24 21:32:32 +0530

    initialize Shell_Cli project with Maven setup and basic application structure

---
+++ Shell_Cli/src/main/java/com/example/Shell_Cli/ShellCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.Shell_Cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellCliApplication.class, args);
+	}
+
+}
---
+++ Shell_Cli/src/test/java/com/example/Shell_Cli/ShellCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shell_Cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
